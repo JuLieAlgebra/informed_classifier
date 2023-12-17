101fd3438e349d8ad28eab586ff150be60775acb
Julieanna Bacon <julieannabacon2014@gmail.com>
2023-12-16 21:04:52 -0500

    neural network: Continued work on implementing the neural network baseline model in pytorch, including work on dataloader

--- informed_classification/naive_ml.py
+++ informed_classification/naive_ml.py
@@ -3,6 +3,8 @@ Demonstration of typical ML solution when there is no information available abou
 
 Neural network.
 """
+import glob
+
 import torch
 from torch import nn
 
@@ -25,5 +27,40 @@ class NeuralNetwork(nn.Module):
         return logits
 
 
-model = NeuralNetwork().to(device)
-print(model)
+class CustomDataset:
+    def __init__(self, file_dir: str):
+        # File dir should be "test", "train", "validation"
+        self.filenames = glob.glob(os.path.join(file_dir, "*"))
+
+    def __len__(self):
+        return len(self.filenames)
+
+    def __get_item__(self, idx):
+        datapoint = np.load(self.filenames[idx])
+        label = int(datapoint[-1])
+        sample = datapoint[:-1]
+        return sample, label
+
+
+def test(model):
+    model.eval()
+    torch.set_grad_enabled(False)
+
+
+def train(model, dataloader, epochs: int):
+    model.train()
+    torch.set_grad_enabled(True)
+
+    for epoch in epochs:
+        for datapoint in dataloader:
+            torch.cuda.synchronize()
+            loss.backward()
+            optimizer.step()
+            torch.cuda.synchronize()
+
+
+if __name__ == "__main__":
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    print(f"Using {device}")
+    model = NeuralNetwork().to(device)
+    print(model)
