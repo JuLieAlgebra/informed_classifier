Gaussian models: Added model that only fits the mean of the data, assumes covariance matrix is known. Added improved example to file. Updated documentation of classes. Fixed bug in experiment name update. Added verbosity to CI testing workflow

informed_classification/generative_models.py
@@ -18,11 +18,11 @@ class GenerativeModel(ABC):
         self.dim = int(dim)
 
     @abstractmethod
-    def p(x: np.array) -> float:
+    def p(self, x: np.array) -> float:
         raise NotImplementedError
 
     @abstractmethod
-    def sample(n: int) -> list[np.array]:
+    def sample(self, n: int) -> list[np.array]:
         raise NotImplementedError
 
 

informed_classification/models.py
@@ -10,19 +10,19 @@ from informed_classification.generative_models import (
 )
 
 
-def regularize_singular_cov(matrix: np.array):
+def regularize_singular_cov(matrix: np.array, a_min: int = 1e-6):
     """We know that the covariance matrix of this problem is not degenerate/singular.
     By doing an unconstrained MLE and then clipping, or projecting, the covariance MLE
     to non-singular matrices, we can think of this as enforcing that prior.
     """
     eig_val, eig_vec = np.linalg.eigh(matrix)
-    eig_val = np.clip(eig_val, a_min=1e-6, a_max=np.inf)
+    eig_val = np.clip(eig_val, a_min=a_min, a_max=np.inf)
     return eig_vec @ np.diag(eig_val) @ eig_vec.T
 
 
 class FittedGaussianModel(GenerativeModel):
     """
-    Fits time-varying mean and time-varying covariance of nominal model.
+    Fits mean and covariance of input data.
     Assumes underlying process is a Gaussian Process/Multivariate Normal.
     """
 
@@ -55,9 +55,32 @@ class FittedGaussianModel(GenerativeModel):
         return self.dist.rvs(size=n).reshape((n, self.dim))
 
 
-class BayesFittedGaussianModel(GenerativeModel):
+class FittedMeanGaussianModel(FittedGaussianModel):
     """
-    Fits time-varying mean and time-varying covariance of nominal model.
+    Fits mean of input data, uses true process's covariance.
+    Assumes underlying process is a Gaussian Process/Multivariate Normal.
+    """
+
+    def __init__(self, data: np.array, process_type: str):
+        model_lookup = {"nominal": NominalModel, "disrupted": DisruptedModel}
+        self.mean_vec = self.mle(data)
+        self.cov_mat = model_lookup[process_type](dim=data.shape[1]).dist.cov
+
+        self.dist = ss.multivariate_normal(
+            mean=self.mean_vec,
+            cov=self.cov_mat,
+            allow_singular=False,
+        )
+        self.dim = self.mean_vec.shape[0]
+
+    def mle(self, data: np.array):
+        mean_vec = np.mean(data, axis=0)
+        return mean_vec
+
+
+class BayesFittedGaussianModel(FittedGaussianModel):
+    """
+    Fits mean and covariance of nominal model.
     Assumes underlying process is a Gaussian Process/Multivariate Normal.
     """
 
@@ -86,25 +109,34 @@ class BayesFittedGaussianModel(GenerativeModel):
 
         return mean_vec, cov_mat
 
-    def p(self, x: np.array) -> float:
-        """probability density at x"""
-        return self.dist.pdf(x)
-
-    def sample(self, n: int) -> list[np.array]:
-        """samples from the nominal model"""
-        return self.dist.rvs(size=n).reshape((n, self.dim))
-
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
     dim = 100
-    n_samples = np.arange(1, 10)  # [10, 20, 50]
+    n_samples = np.arange(1, 10, step=2)
     for n in n_samples:
         underlying_model = NominalModel(dim=dim)
         data = underlying_model.sample(n)
 
         model = FittedGaussianModel(data=data)
-        print(model.p(data))
-        plt.plot(model.sample(10).T)
+        mean_model = FittedMeanGaussianModel(data=data, process_type="nominal")
+
+        ## Plotting the results
+        fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+        fig.suptitle(f"Samples from Fitted GP Models for n={n}")
+
+        # Plot for FittedGaussianModel
+        axes[0].plot(model.sample(10).T)
+        axes[0].set_title("Samples from Mean and Cov Fitted GP")
+        axes[0].set_xlabel("Dimension")
+        axes[0].set_ylabel("Value")
+
+        # Plot for FittedMeanGaussianModel
+        axes[1].plot(mean_model.sample(10).T)
+        axes[1].set_title("Samples from Mean Fitted GP")
+        axes[1].set_xlabel("Dimension")
+        axes[1].set_ylabel("Value")
+
+        plt.tight_layout(rect=[0, 0.03, 1, 0.95])
         plt.show()