099e969d2a951c1c6a96505edf1ebd8180189072
Julieanna Bacon <julieannabacon2014@gmail.com>
2023-12-17 21:16:50 -0500

    Fitted models: Added model that estimates the covariance from data alone, updated example

--- informed_classification/models.py
+++ informed_classification/models.py
@@ -78,36 +78,31 @@ class FittedMeanGaussianModel(FittedGaussianModel):
         return mean_vec
 
 
-class BayesFittedGaussianModel(FittedGaussianModel):
+class FittedCovGaussianModel(FittedGaussianModel):
     """
-    Fits mean and covariance of nominal model.
+    Fits covariance of input data.
     Assumes underlying process is a Gaussian Process/Multivariate Normal.
     """
 
-    def __init__(self, data: np.array, prior: np.array):
-        self.mean_vec, self.cov_mat = self.maximum_aposterior(self.mle(data), prior)
+    def __init__(self, data: np.array, process_type: str):
+        model_lookup = {"nominal": NominalModel, "disrupted": DisruptedModel}
+        self.mean_vec = model_lookup[process_type](dim=data.shape[1]).dist.mean
+        self.cov_mat = self.mle(data)
 
         self.dist = ss.multivariate_normal(
             mean=self.mean_vec,
             cov=self.cov_mat,
-            allow_singular=True,
+            allow_singular=False,
         )
         self.dim = self.mean_vec.shape[0]
 
-    def maximum_aposterior(self, mle, prior):
-        """ """
-        pass
-
     def mle(self, data: np.array):
         if data.shape[0] == 1:
-            cov_mat = np.zeros(data.shape[1])
+            cov_mat = np.zeros((data.shape[1], data.shape[1]))
         else:
             cov_mat = np.cov(data, rowvar=False)
-        mean_vec = np.mean(data, axis=0)
 
-        assert cov_mat.shape[0] == mean_vec.shape[0]
-
-        return mean_vec, cov_mat
+        return regularize_singular_cov(cov_mat)
 
 
 if __name__ == "__main__":
@@ -120,23 +115,44 @@ if __name__ == "__main__":
         data = underlying_model.sample(n)
 
         model = FittedGaussianModel(data=data)
-        mean_model = FittedMeanGaussianModel(data=data, process_type="nominal")
+        mean_nominal_model = FittedMeanGaussianModel(data=data, process_type="nominal")
+        mean_disrupted_model = FittedMeanGaussianModel(
+            data=data, process_type="disrupted"
+        )
+        cov_nominal_model = FittedCovGaussianModel(data=data, process_type="nominal")
+        cov_disrupted_model = FittedCovGaussianModel(
+            data=data, process_type="disrupted"
+        )
 
         ## Plotting the results
-        fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+        fig, axes = plt.subplots(1, 5, figsize=(23, 5))
         fig.suptitle(f"Samples from Fitted GP Models for n={n}")
 
-        # Plot for FittedGaussianModel
+        # FittedGaussianModel
         axes[0].plot(model.sample(10).T)
-        axes[0].set_title("Samples from Mean and Cov Fitted GP")
+        axes[0].set_title("Mean and Cov Fitted GP")
         axes[0].set_xlabel("Dimension")
         axes[0].set_ylabel("Value")
 
-        # Plot for FittedMeanGaussianModel
-        axes[1].plot(mean_model.sample(10).T)
-        axes[1].set_title("Samples from Mean Fitted GP")
+        # FittedMeanGaussianModel (Nominal)
+        axes[1].plot(mean_nominal_model.sample(10).T)
+        axes[1].set_title("Mean Fitted GP (Nominal)")
         axes[1].set_xlabel("Dimension")
-        axes[1].set_ylabel("Value")
+
+        # FittedMeanGaussianModel (Disrupted)
+        axes[2].plot(mean_disrupted_model.sample(10).T)
+        axes[2].set_title("Mean Fitted GP (Disrupted)")
+        axes[2].set_xlabel("Dimension")
+
+        # FittedCovGaussianModel (Nominal)
+        axes[3].plot(cov_nominal_model.sample(10).T)
+        axes[3].set_title("Cov Fitted GP (Nominal)")
+        axes[3].set_xlabel("Dimension")
+
+        # FittedCovGaussianModel (Disrupted)
+        axes[4].plot(cov_disrupted_model.sample(10).T)
+        axes[4].set_title("Cov Fitted GP (Disrupted)")
+        axes[4].set_xlabel("Dimension")
 
         plt.tight_layout(rect=[0, 0.03, 1, 0.95])
         plt.show()
